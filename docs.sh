d4273b832de0c22eede65646a421bba696af7bf6
oliverpauffley <mrpauffley@gmail.com>
2023-06-05 14:01:27 +0100

    fix docs

--- docs.sh
+++ docs.sh
@@ -2,5 +2,5 @@
 
 cargo doc --no-deps
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=poc_offtake_parser\">" > target/doc/index.html
 cp -r target/doc ./docs
